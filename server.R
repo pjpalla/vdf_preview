c6590d5108b323f0c981da3d17afb2694dc874b6
pg <palla.pg@gmail.com>
2020-02-03 14:10:10 +0100

    add color palette

--- server.R
+++ server.R
@@ -28,9 +28,10 @@ source("R/input_output.R")
 raw_io_min = read.csv("kpi/io/io_data_min.csv")
 mappings <- read.csv("mappings/multimap_ras.csv", sep = ";")
 adr <- readOGR("shapefiles/MULTIMAP.shp")
-adr2 <- adr[adr$MAP_ID == 2, ]
-x = as.character(adr2$AREA_LB_0)
-adr2$AREA_LB_0 = as.factor(x)
+#adr <- readOGR("shapefiles/map3.shp")
+# adr2 <- adr[adr$MAP_ID == 2, ]
+# x = as.character(adr2$AREA_LB_0)
+# adr2$AREA_LB_0 = as.factor(x)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -49,7 +50,7 @@ shinyServer(function(input, output) {
   output$areas <- renderLeaflet({
     
     ### here we create the aggregated dataset with the arrivals to use to draw the map
-    aggregated_inputs <- get_input_by_adr(dataframe = raw_io_min, map_value = 3, kpi_value = "arrivals", month_selected = 2, threshold = 0.005)
+    aggregated_inputs <- get_input_by_adr(dataframe = raw_io_min, map_value = 3, kpi_value = "arrivals", month_selected = 3, threshold = 0.005)
     
     ## now we add the arrivals to the adr map
     adr <- adr[adr$MAP_ID == 3, ]
@@ -58,12 +59,20 @@ shinyServer(function(input, output) {
     adr$arrivals <- sapply(adr$AREA_LB_0, function(x) aggregated_inputs$filtered_arrivals[aggregated_inputs$adr_name == x])
     
     ### here we define colours
-    reds <- colorRampPalette(brewer.pal(9, "Reds"))(6)
-    pal <- colorNumeric(reds, domain = adr$filtered_arrivals)
+#    reds <- colorRampPalette(brewer.pal(9, "Reds"))(10)
+    #pal <- colorNumeric(reds[3:10], domain = adr$filtered_arrivals)
+    #adr <- adr[adr$arrivals > 0, ]
+    top_values <- sort(adr$arrivals, decreasing = T)[1:16]
+    pal <- colorNumeric(
+      palette = "Blues",
+      domain = top_values)
     
+    #pal <- colorBin("Reds", adr$arrivals, 4, pretty = F)
+    
+    #qpal <- colorQuantile("Blues", sort(adr$arrivals, decreasing = T)[1:20], n = 9)
     
     m <- leaflet(data = adr) %>% setView(lng=8.981, lat=40.072, zoom=8) %>% 
-              addPolygons(layerId = adr$AREA_LB_0, color = "#444444", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.5, fillColor = ~pal(arrivals),
+              addPolygons(layerId = adr, color = "#444444", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 1, fillColor = ~pal(arrivals),
                           highlightOptions = highlightOptions(color = "white", weight = 2,
                                                               bringToFront = TRUE), label = adr$AREA_LB_0, labelOptions = labelOptions(clickable = FALSE, noHide = FALSE))              
               
