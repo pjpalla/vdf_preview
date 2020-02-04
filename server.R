302d87a044e9d6a9c3ce37b04a3613a8bb7a0278
Pg <palla.pg@gmail.com>
2020-02-04 08:38:46 +0100

    create user interface to visualise maps

--- server.R
+++ server.R
@@ -47,34 +47,59 @@ shinyServer(function(input, output) {
     
   })
   
+  output$arrivals <- renderTable({
+            map_input = as.integer(input$map_choice)
+            month_input = as.integer(input$month)
+            ### here we create the aggregated dataset with the arrivals to use to draw the map
+            aggregated_inputs <- get_input_by_adr(dataframe = raw_io_min, map_value = map_input, kpi_value = "arrivals", month_selected = month_input, threshold = 0)     
+            aggregated_inputs$arrivals <- as.integer(aggregated_inputs$arrivals)
+            aggregated_inputs$percentage <- paste(round(aggregated_inputs$percentage,3)*100, "%")
+            names(aggregated_inputs)[2] = "ingressi"
+            names(aggregated_inputs)[3] = "adr"
+            names(aggregated_inputs)[4] = "percentuale"            
+            # aggregated_inputs %>% rename(
+            #           ingressi = arrivals,
+            #           adr = adr_name,
+            #           percentuale = percentage
+            # )
+            aggregated_inputs <- aggregated_inputs[, c(2, 3, 4)]
+            aggregated_inputs <- head(aggregated_inputs[])          
+            }) 
+  
   output$areas <- renderLeaflet({
     
+    map_input = as.integer(input$map_choice)
+    month_input = as.integer(input$month)
+    input_threshold = as.numeric(input$threshold/100)
+    print(input_threshold)
+    
     ### here we create the aggregated dataset with the arrivals to use to draw the map
-    aggregated_inputs <- get_input_by_adr(dataframe = raw_io_min, map_value = 3, kpi_value = "arrivals", month_selected = 3, threshold = 0.005)
+    aggregated_inputs <- get_input_by_adr(dataframe = raw_io_min, map_value = map_input, kpi_value = "arrivals", month_selected = month_input, threshold = input_threshold)
     
     ## now we add the arrivals to the adr map
-    adr <- adr[adr$MAP_ID == 3, ]
+    adr <- adr[adr$MAP_ID == map_input, ]
     adr_levels = as.character(adr$AREA_LB_0)
     adr$AREA_LB_0 = as.factor(adr_levels)
     adr$arrivals <- sapply(adr$AREA_LB_0, function(x) aggregated_inputs$filtered_arrivals[aggregated_inputs$adr_name == x])
-    
+    adr <- adr[adr$arrivals > 0, ]
     ### here we define colours
 #    reds <- colorRampPalette(brewer.pal(9, "Reds"))(10)
     #pal <- colorNumeric(reds[3:10], domain = adr$filtered_arrivals)
     #adr <- adr[adr$arrivals > 0, ]
-    top_values <- sort(adr$arrivals, decreasing = T)[1:16]
+    #top_values <- sort(adr$arrivals, decreasing = T)[1:16]
     pal <- colorNumeric(
-      palette = "Blues",
-      domain = top_values)
+      palette = brewer.pal(n=9, "PuBuGn")[3:9],
+      domain = NULL)
+
+    #pal <- colorBin("Blues", 4, pretty = F)
     
-    #pal <- colorBin("Reds", adr$arrivals, 4, pretty = F)
+#    qpal <- colorQuantile("Blues", c(min(top_values), max(top_values)), n = 4)
     
-    #qpal <- colorQuantile("Blues", sort(adr$arrivals, decreasing = T)[1:20], n = 9)
-    
-    m <- leaflet(data = adr) %>% setView(lng=8.981, lat=40.072, zoom=8) %>% 
+    m <- leaflet(data = adr) %>% setView(lng=8.981, lat=40.072, zoom=8) %>% addTiles() %>%
               addPolygons(layerId = adr, color = "#444444", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 1, fillColor = ~pal(arrivals),
                           highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                              bringToFront = TRUE), label = adr$AREA_LB_0, labelOptions = labelOptions(clickable = FALSE, noHide = FALSE))              
+                                                              bringToFront = TRUE), label = paste(adr$AREA_LB_0, ":", adr$arrivals), labelOptions = labelOptions(clickable = FALSE, noHide = FALSE)) %>%
+              addLegend("bottomright", pal = pal, values = ~arrivals, title = "ingressi", opacity = 1)
               
     m
   })
--- ui.R
+++ ui.R
@@ -8,33 +8,54 @@
 #
 
 library(shiny)
-library(leaflet)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  fluidRow(
-    column( 
-      width = 6,
-      leafletOutput("areas", height = "600")
-    ))
-  
-  # Application title
-  # titlePanel("Old Faithful Geyser Data"),
+  # fluidRow(
+  #           sidebarPanel(
+  #                     sliderInput("bins",
+  #                                 "Number of bins:",
+  #                                 min = 1,
+  #                                 max = 50,
+  #                                 value = 30)
+  #           ),            
+  #             column(
+  #               width = 6,
+  #               leafletOutput("areas", height = "600")
+  #             ))
   # 
-  # # Sidebar with a slider input for number of bins 
-  # sidebarLayout(
-  #   sidebarPanel(
-  #      sliderInput("bins",
-  #                  "Number of bins:",
-  #                  min = 1,
-  #                  max = 50,
-  #                  value = 30)
-  #   ),
-  #   
-  #   # Show a plot of the generated distribution
-  #   mainPanel(
-  #      plotOutput("distPlot")
-  #   )
-  # )
+  # Application title
+  div(h2("Punti di accesso alla Sardegna: distribuzione ingressi")),br(),
+            
+  #titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+       radioButtons("map_choice", "Seleziona mappa:",
+                           c("Mappa 3 (Comuni + POI)" = 3,
+                             "Mappa 4 (Province + POI)" = 4)), br(),br(),
+       selectInput("month", "Seleziona mese:",
+                   c("Febbraio" = 2,
+                     "Marzo" = 3,
+                     "Aprile" = 4,
+                     "Maggio" = 5,
+                     "Giugno" = 6), selected = 3), br(),br(),br(),
+       sliderInput("threshold",
+                   "Soglia arrivi (%):",
+                   min = 0,
+                   max = 5,
+                   value = 0.5, step = 0.1),br(), br(),
+       
+       h3("Ingressi in Sardegna"),br(),
+       tableOutput("arrivals")
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+       leafletOutput("areas", height = "800")                
+       #plotOutput("distPlot")
+    )
+  )
 ))
